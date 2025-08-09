f586c0e7e9da2de6126b4f28cd21a09cbeb4bcff
elektrox <elektrox@gmail.com>
2025-08-09 11:35:28 +0000

    uwb simplification

--- src/uwb/android/src/main/kotlin/net/christiangreiner/uwb/oob/UwbConfig.kt
+++
@@ -1,4 +0,0 @@
-package net.christiangreiner.uwb.oob
-
-// A simple data class to hold UWB configuration parameters within the native Android code.
-data class UwbConfig(val preambleIndex: Int, val sessionKey: Int, val uwbAddress: ByteArray)
